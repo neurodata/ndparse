3ca0b2c9db6461d08f3b2f9df020c0240fae4d3e
Will Gray Roncal <wgr@jhu.edu>
2016-02-25 21:02:31 -0500

    pip v.0.0.2

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
-from distutils.core import setup
 import ndparse
 
 VERSION = ndparse.version
 
+from distutils.core import setup
 setup(
-    name = 'ndparse',
-    packages = [
-        'ndparse'
-        '.annotate',
-        #'.algorithms',
-        #'.learn',
-        #'.deploy'
-        #'.utils'
-
+    name='ndparse',
+    packages=[
+        'ndparse',
+        'ndparse.annotate',
+        'ndparse.algorithms',
+        'ndparse.learn',
+        'ndparse.deploy',
+        'ndparse.utils'
     ],
-    version = VERSION,
-    description = 'A Python library for NeuroData computer vision and data processing',
-    author = 'William Gray Roncal',
-    author_email = 'wgr@jhu.edu',
-    url = 'https://github.com/neurodata/ndparse',
+    version=VERSION,
+    description='A Python library for NeuroData computer vision and data processing',
+    author='William Gray Roncal',
+    author_email='wgr@jhu.edu',
+    url='https://github.com/neurodata/ndparse',
     download_url = 'https://github.com/neurodata/ndparse/tarball/' + VERSION,
-    keywords = [
+    keywords=[
         'NeuroData',
         'object detection',
         'annotation',
