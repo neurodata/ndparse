15a28d9e84bb713812396b2a55908fc586c7a092
gkiar <gkiar07@gmail.com>
2016-02-29 08:37:46 -0500

    added concatenate csv function

--- ndparse/utils/utils.py
+++ ndparse/utils/utils.py
@@ -1,7 +1,14 @@
 import numpy as np
 
+
 def compute_centroids(data):
 
     centroids = 5
 
     return centroids
+
+
+def concatenate_files(files, outf):
+    with open(outf, "a") as fout:
+        [fout.write(line) for f in files for line in open(f)]
+    pass
